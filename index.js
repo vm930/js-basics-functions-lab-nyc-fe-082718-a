0f01f4b614d14fc0a3d54d4ccbdb5e7f3d9512c0
abstract-animation-9914 <vm930@hotmail.com>
2018-10-16 00:14:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+
+function distanceTravelledInBlocks(location){
+  return abs(42 - location);
+}
+const headquater = 42;
+
+function absolute(number){
+  if (number < 0){
+    return number* -1;
+  }
+  return number;
+}
+
+//distanceFromHqInBlocks: Returns the number of blocks
+//from Scuber's headquarters to the pickup location.
+
+function distanceFromHqInBlocks() (headquater){
+  //1 block === 264 
+  return distanceTravelledInBlocks * 264;
+}
+//distanceFromHqInFeet: Returns the number of feet from //Scuber's 
+
+function distanceTravelledInFeet (start, end){
+  return absolute(end - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  if (absolute(destination - start)>400){
+    //if the distance is less than 400 feet return 0
+    return 0;
+  } else if {
+    
+  }
+}
